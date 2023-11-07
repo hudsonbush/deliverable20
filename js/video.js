5dee867a662c147a709acdf6bbde2e774503bed9
hfbush <hfbush@umich.edu>
2023-11-07 09:47:12 -0500

    first commit

--- js/video.js
+++ js/video.js
@@ -1,11 +1,26 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	video = document.querySelector('#player1');
+	video.autoplay = false;
+	video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	document.querySelector('#volume').innerHTML = document.querySelector('#slider').value;
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate -= 0.1;
+	console.log(video.playbackRate); 
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate += 0.1;
+	console.log(video.playbackRate); 
+});
