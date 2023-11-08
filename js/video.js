9c01d810f95c835521b4b7de973db48ca88ffbeb
hfbush <hfbush@umich.edu>
2023-11-08 09:40:39 -0500

    everything done but slider

--- js/video.js
+++ js/video.js
@@ -24,3 +24,36 @@ document.querySelector("#faster").addEventListener("click", function() {
 	video.playbackRate += 0.1;
 	console.log(video.playbackRate); 
 });
+
+document.querySelector("#skip").addEventListener("click", function () {
+	var timestamp = video.currentTime + 10;
+	if(timestamp > video.duration){
+		video.currentTime = 0;
+	}else{
+		video.currentTime = timestamp;
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function(){
+	if(video.muted == true){
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}else{
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+});
+
+// var rangeInput = document.querySelector("#slider").value;
+// rangeInput.addEventListener("change", function(){
+// 	video.volume = (rangeInput/100);
+// 	document.querySelector('#volume').innerHTML = rangeInput;
+// });
+
+document.querySelector("#vintage").addEventListener("click", function(){
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function(){
+	video.classList.remove("oldSchool");
+});
